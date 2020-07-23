91be4f519c0cd4f023ad3036e45132110608ae15
Magno Biét <magno.biet@gmail.com>
2020-07-23 19:44:25 -0300

    chore: initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '499857f104df870daab872e37a70dbcc'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f5ef04d6-e139-454a-9b3c-9ae80866b2e9'
+};
